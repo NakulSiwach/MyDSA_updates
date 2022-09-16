f515ea87efe0f8bf3235dda096f568f5fa455992
Nakul Siwach <64941476+NakulSiwach@users.noreply.github.com>
2022-09-16 23:39:50 +0530

    Create Bitwise_operators.cpp
    all about bitwise operators.

---
+++ Bitwise_operators.cpp
@@ -0,0 +1,9 @@
+
+
+5<<1 left shift
+what it does is moves all 1 bits(set bits) to left, multiply by 2.
+opposite with right shift.
+
+paddings are done with 0 always.
+  
+xor ^ , when bits are same , it reverses , x^x = 0.
