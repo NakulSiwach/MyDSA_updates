5a220257c4ad2619613b855b0cb7415195d90ac4
Nakul Siwach <64941476+NakulSiwach@users.noreply.github.com>
2022-09-19 21:48:55 +0530

    Create Permutations.cpp

---
+++ Permutations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void helper(vector<int>nums,vector<vector<int>>&ans,int n){
+        if (n==nums.size()){
+            ans.push_back(nums);
+        }
+        for (int i = n ; i < nums.size();i++){
+            swap(nums[i],nums[n]);
+            helper(nums,ans,n+1);
+            swap(nums[i],nums[n]);
+        }
+        return;
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        helper(nums,ans,0);
+        return ans;
+    }
+};
