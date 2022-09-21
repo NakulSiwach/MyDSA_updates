bf844993ccda447fb2b7a26763e2d448d276ac8c
Nakul Siwach <64941476+NakulSiwach@users.noreply.github.com>
2022-09-21 20:46:11 +0530

    Update 01knapsack_DP.cpp

--- 01knapsack_DP.cpp
+++ 01knapsack_DP.cpp
@@ -1,4 +1,5 @@
 
+// top-down (memoized)
 class Solution
 {
     public:
@@ -29,3 +30,39 @@ class Solution
         return helper(W,wt,val,n,dp);
     }
 };
+
+
+// bottom-up (tabulation)
+
+class Solution
+{
+    public:
+    //Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       int dp[n+1][W+1];
+       for(int i =0 ; i<n+1;i++){
+        for(int j =0 ;j<W+1;j++){
+            if(i==0 or j==0){
+                dp[i][j] = 0;
+            }
+        }
+       }
+
+       for(int i =1 ; i<n+1;i++){
+        for(int j =1 ;j<W+1;j++){
+            if(wt[i-1]-j<=0){
+                dp[i][j] = max(dp[i-1][j],val[i-1]+dp[i-1][j-wt[i-1]]);
+            }
+            else{
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+       }
+       return dp[n][W];
+
+    }
+};
+
+
+
