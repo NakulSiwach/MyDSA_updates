9776635d168ef27332356d79437424b1be0022f8
Nakul Siwach <64941476+NakulSiwach@users.noreply.github.com>
2022-09-22 15:20:26 +0530

    Update SubsetSum_DP.cpp

--- SubsetSum_DP.cpp
+++ SubsetSum_DP.cpp
@@ -1,3 +1,39 @@
+//this below code is of equal partition (this uses subset sum)
+class Solution {
+public:
+    bool canPartition(vector<int>& arr) {
+        int sum = accumulate(arr.begin(),arr.end(),0);
+        if (sum%2!=0){
+            return false;
+        }
+        int N = arr.size();
+        
+        bool dp[1001][(sum)+1];
+        sum = sum/2;
+
+        for(int i = 0 ; i <= sum;i++){
+            dp[0][i] = false;
+        }
+        for(int i =0 ; i <= N;i++){
+            dp[i][0] = true;
+        }
+
+        for(int i = 1 ; i <=N;i++){
+            for(int j = 1 ; j <= sum;j++){
+                if(j-arr[i-1]>=0){
+                    dp[i][j] = dp[i-1][j] or dp[i-1][j-arr[i-1]];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[N][sum];
+    }
+};
+
+
+
 // recursion
 
 // wrong codeeee
@@ -25,7 +61,6 @@ class Solution
         return helper(W,wt,val,n);
     }
 };
-
 
 // memoized
 class Solution
