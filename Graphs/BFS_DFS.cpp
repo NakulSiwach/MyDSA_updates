8b9a928525d604f67aa43bd52822dedb389a19f7
Nakul Siwach <64941476+NakulSiwach@users.noreply.github.com>
2022-09-26 17:11:15 +0530

    Update BFS_DFS.cpp

--- Graphs/BFS_DFS.cpp
+++ Graphs/BFS_DFS.cpp
@@ -1,1 +1,38 @@
-//
+// BFS
+#include<bits/stdc++.h>
+using namespace std;
+
+void helper(unordered_map<int,vector<int>>adj,unordered_map<int,bool>&vis,int i){
+	vis[i] = true;
+	queue<int>q;
+	q.push(i);
+	while(!q.empty()){
+		int t = q.front();
+		cout<<t;
+		q.pop();
+		for(auto i:adj[t]){
+			if (vis[i]!=true){
+				q.push(i);
+				
+				vis[i] = true;
+			}
+		}
+	}
+	return;
+}
+
+int main(){
+	unordered_map<int,vector<int>>adj;
+	adj[0].push_back(1);
+	adj[1].push_back(2);
+	adj[1].push_back(0);
+	adj[2].push_back(3);
+	adj[2].push_back(4);
+	adj[3].push_back(4);
+	adj[3].push_back(2);
+	adj[4].push_back(2);
+	adj[4].push_back(3);
+	unordered_map<int,bool>vis;
+	helper(adj,vis,0);
+	
+}
